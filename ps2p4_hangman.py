6cc6a12be22e2dda02d31d276b6820653be60281
BoulderOWASP <christopher.rossi@owasp.org>
2013-07-31 17:58:46 -0600

    Part of the way through creating the hangman program

--- ps2p4_hangman.py
+++ ps2p4_hangman.py
@@ -43,6 +43,32 @@ def choose_word(wordlist):
 # actually load the dictionary of words and point to it with 
 # the wordlist variable so that it can be accessed from anywhere
 # in the program
-wordlist = load_words()
 
+
+wordlist = load_words()
 # your code begins here!
+guesses_left = 8
+
+def hangman():
+    random_word_pick = choose_word(wordlist)
+    return random_word_pick
+
+def guess():
+    guess = raw_input('Please guess a letter:')
+    
+    print 'Good guess:'
+    for i in len(random_word_pick):
+        if guess == i:
+            print guess
+        else:
+            print '_'
+    print '----------'
+    guesses_left =- 1
+
+
+
+random_word_pick = hangman()
+print 'Welcome to the game, Hangman!\nI am thinking of a word that is',
+print len(random_word_pick), 'letters long.\n-------------\n You have', guesses_left, 'guesses left.'
+
+print random_word_pick
