83f872087d53bb233f431a9d9f75fa5bffbe2f0c
BoulderOWASP <christopher.rossi@owasp.org>
2013-10-06 16:11:13 -0600

    KBCO Scrape Code

---
+++ kbco_scrape.py
@@ -0,0 +1,20 @@
+##KBCO Code
+##Name: CRossi
+##Program to scrape the KBCO look_for_your_name page for my name, and email me if it is there
+import urllib2
+import re
+from bs4 import BeautifulSoup
+
+def check_name():
+    soup = BeautifulSoup(urllib2.urlopen('http://www.kbco.com/pages/lookforyourname.html').read())
+
+    name_search = soup.find_all(text=re.compile("Christopher Rossi"), limit = 1)
+    if name_search != []:
+        return True
+
+print check_name()
+
+
+
+
+
