6a5a780e84f8e83620d19831c6c608ef5c4403d8
crossi <crossi@crossiweb01.(none)>
2014-01-28 23:25:20 -0500

    changed to current working code without beautiful soup

--- kbco_scrape.py
+++ kbco_scrape.py
@@ -3,17 +3,16 @@
 ##Program to scrape the KBCO look_for_your_name page for my name, and email me if it is there
 import urllib2
 import re
-from bs4 import BeautifulSoup
 
 def check_name():
-    soup = BeautifulSoup(urllib2.urlopen('http://www.kbco.com/pages/lookforyourname.html').read())
+    fucksoup = urllib2.urlopen('http://www.kbco.com/pages/lookforyourname.html').read()
 
-    name_search = soup.find_all(text=re.compile("Matthew Hooker"), limit = 1)
-    if name_search != []:
+    if (re.search(r"Christopher Rossi", fucksoup, re.IGNORECASE)):
         return True
+    return False
 
-print check_name()
-
+if check_name():
+    print "you're a winner ho!"
 
 
 
